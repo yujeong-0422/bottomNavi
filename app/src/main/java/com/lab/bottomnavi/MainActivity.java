06c1c59428176873214c822267b86e3677f1076d
yujeong-0422 <pride0422@naver.com>
2022-09-13 21:13:17 +0900

    first

---
+++ app/src/main/java/com/lab/bottomnavi/MainActivity.java
@@ -0,0 +1,98 @@
+package com.lab.bottomnavi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private frag_home frag_home;
+    private frag_info frag_info;
+    private frag_setting frag_setting;
+    private frag_notification frag_notification;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NotNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+
+                    case R.id.home:
+                        setFrag(0);
+                        break;
+
+                    case R.id.notification:
+                        setFrag(1);
+                        break;
+
+                    case R.id.setting:
+                        setFrag(2);
+                        break;
+
+                    case R.id.info:
+                        setFrag(3);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+
+
+        frag_home = new frag_home();
+        frag_info = new frag_info();
+        frag_notification = new frag_notification();
+        frag_setting = new frag_setting();
+        setFrag(0);
+
+    }
+
+    private void setFrag(int n) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch (n) {
+            case 0:
+                ft.replace(R.id.main_frame, frag_home);
+                ft.commit();
+                break;
+
+            case 1:
+                ft.replace(R.id.main_frame, frag_notification);
+                ft.commit();
+                break;
+
+            case 2:
+                ft.replace(R.id.main_frame, frag_info);
+                ft.commit();
+                break;
+
+            case 3:
+                ft.replace(R.id.main_frame, frag_setting);
+                ft.commit();
+                break;
+
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/lab/bottomnavi/frag_setting.java
@@ -0,0 +1,20 @@
+package com.lab.bottomnavi;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class frag_setting extends Fragment {
+
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.frag_setting, container, false);
+
+        return view;
+    }
+}
